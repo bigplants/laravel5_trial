4ddea258230a7c411bb4de9089c2d715f1112a07
Daiki Hirakata <hirakatad@isao.co.jp>
2015-07-04 12:18:42 +0900

    exec ide-helper:models.

--- app/Article.php
+++ app/Article.php
@@ -4,6 +4,20 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Article
+ *
+ * @property integer $id 
+ * @property string $title 
+ * @property string $body 
+ * @property \Carbon\Carbon $created_at 
+ * @property \Carbon\Carbon $updated_at 
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereTitle($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereBody($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Article whereUpdatedAt($value)
+ */
 class Article extends Model
 {
     /**
