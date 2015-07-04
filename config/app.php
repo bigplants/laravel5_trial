f99e806ca307ea6ca1b361c6678600808ac8b5ab
Daiki Hirakata <hirakatad@isao.co.jp>
2015-07-04 12:30:40 +0900

    add html and debugger

--- config/app.php
+++ config/app.php
@@ -136,6 +136,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Illuminate\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -147,6 +148,8 @@ return [
          * IDE helper
          */
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        //Debugger
+        Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -195,6 +198,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Illuminate\Html\FormFacade::class,
+        'Html' => Illuminate\Html\HtmlFacade::class,
+        'Debugger' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
