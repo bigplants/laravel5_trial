68ecf1465111e302b033450ae5e5dc4ef0a0e7b4
Daiki Hirakata <hirakatad@isao.co.jp>
2015-07-13 13:47:45 +0900

    add front end task for gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,25 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function (mix) {
-    mix.sass('app.scss');
+    mix.sass('app.scss')
+        .copy(
+        'vendor/bower_components/jquery/dist/jquery.min.js',
+        'public/js/vendor/jquery.js'
+    )
+        .copy(
+        'vendor/bower_components/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+        'public/js/vendor/bootstrap.js'
+    )
+        .copy(
+        'vendor/bower_components/font-awesome/css/font-awesome.min.css',
+        'public/css/vendor/font-awesome.css'
+    )
+        .copy(
+        'vendor/bower_components/font-awesome/fonts',
+        'public/fonts'
+    )
+        .copy(
+        'vendor/bower_components/angular/angular.min.js',
+        'public/js/vendor/angular.js'
+    );
 });
